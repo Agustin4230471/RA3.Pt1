a64cba93d6bf911b53481c53ec0951a8f682b9f0
Agustín <agustinruiz@institutvidreres.cat>
2024-11-08 09:46:27 +0100

    Exercici 2

--- src/JavaScript_Variables.js
+++ src/JavaScript_Variables.js
@@ -1,6 +1,15 @@
+//Exercici 1
 let nom = "Agustín"
 let cognom = "Ruiz"
 let dataDeNeixement = "22/9/2004"
 console.log(nom)
 console.log(cognom)
 console.log(dataDeNeixement)
+
+//Exercici 2
+let numero = 3
+let paraula = 'Hola'
+let siONo = false
+console.log(typeof numero)
+console.log(typeof paraula)
+console.log(typeof siONo)
