a347c126f20196851e58f8bbd5626dfb66d908e0
Agustín <agustinruiz@institutvidreres.cat>
2024-11-11 08:27:29 +0100

    Exercici 3

--- src/JavaScript_Variables.js
+++ src/JavaScript_Variables.js
@@ -1,4 +1,4 @@
-//Exercici 1
+//Exercici 1: Mostra per consola el teu nom, cognoms i any de naixement utilitzant diferents missatges de consola.
 let nom = "Agustín"
 let cognom = "Ruiz"
 let dataDeNeixement = "22/9/2004"
@@ -6,10 +6,15 @@ console.log(nom)
 console.log(cognom)
 console.log(dataDeNeixement)
 
-//Exercici 2
+//Exercici 2: Declara 3 variables, cadascuna amb un tipus de dades diferent i mostra el valor per consola.
 let numero = 3
 let paraula = 'Hola'
 let siONo = false
 console.log(typeof numero)
 console.log(typeof paraula)
 console.log(typeof siONo)
+
+//Exercici 3: Mostra una variable per consola que hagis declarat posteriorment amb var. Què apareix? I si canvies la declaració a let ?
+console.log(typeof variable)
+var variable = "Hola"
+//Amb var surt "undefined" i amb let surt un error.
