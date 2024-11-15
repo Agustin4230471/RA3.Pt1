91e35d2adf40284973b12d24ecabebc3d5b8fb4e
Agustín <agustinruiz@institutvidreres.cat>
2024-11-15 09:30:25 +0100

    Exercici 7

--- src/Segon_Arxiu.js
+++ src/Segon_Arxiu.js
@@ -7,3 +7,10 @@ console.log((number + bigIntAsNumber).toFixed(1));
 console.log((number - bigIntAsNumber).toFixed(1));
 console.log((number * bigIntAsNumber).toFixed(1));
 console.log((number / bigIntAsNumber).toFixed(1));
+
+//Exercici 7
+const a = 4
+const b = 4
+const c = (a * a) + (b * a)
+const hipotenusa = Math.sqrt(c)
+console.log(hipotenusa)
